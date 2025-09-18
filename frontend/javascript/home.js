3023ed18be7139c10ce863b446d9c34be40b8a2c
Bgoutami27 <goutamu9438721075@gmail.com>
2025-09-18 15:16:54 +0530

    Update home.js

--- frontend/javascript/home.js
+++ frontend/javascript/home.js
@@ -2,10 +2,11 @@
 
 document.querySelector('.Login').addEventListener('click', () => {
     console.log('Login button clicked!');
-    window.location.assign("http://127.0.0.1:5500/login.html");
+    window.location.assign("https://shoekart1.onrender.com/login.html");
     const select = document.querySelector('.text');
     
 });
+
 
 
 
